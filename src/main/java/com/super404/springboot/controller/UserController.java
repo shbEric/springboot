7a0d1858904a2dd206339707800240fdb7145053
shbEric <1289878918@qq.com>
2019-10-19 22:25:27 +0800

    实现增删改查的简单示例

--- src/main/java/com/super404/springboot/controller/UserController.java
+++ src/main/java/com/super404/springboot/controller/UserController.java
@@ -2,6 +2,7 @@ package com.super404.springboot.controller;
 
 import com.super404.springboot.domain.JsonData;
 import com.super404.springboot.domain.User;
+import com.super404.springboot.mapper.UserMapper;
 import com.super404.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,8 +35,39 @@ public class UserController {
 
         return JsonData.buildSuccess(id);
 
+    }
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 查找全部用户
+     * @return
+     */
+    @GetMapping("findAll")
+    public Object findAll(){
+        return  JsonData.buildSuccess(userMapper.getAll());
+    }
 
 
+    @GetMapping("find_by_id")
+    public Object findById(long id){
+        return JsonData.buildSuccess(userMapper.findById(id));
+    }
+
+    @GetMapping("del_by_id")
+    public Object delById(long id){
+        userMapper.delete(id);
+        return JsonData.buildSuccess();
+    }
+
+    @GetMapping("update")
+    public Object update(String name,int id){
+        User user = new User();
+        user.setName(name);
+        user.setId(id);
+        userMapper.update(user);
+        return JsonData.buildSuccess();
     }
 
 }
--- src/main/java/com/super404/springboot/mapper/UserMapper.java
+++ src/main/java/com/super404/springboot/mapper/UserMapper.java
@@ -1,8 +1,15 @@
 package com.super404.springboot.mapper;
 
 import com.super404.springboot.domain.User;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 /**
  * 访问数据库的接口
@@ -14,5 +21,42 @@ public interface UserMapper {
     //获取数据库自增id，keyProperty是JAVA对象的属性，keyColumn表示数据库的字段
     @Options(useGeneratedKeys=true, keyProperty="id", keyColumn="id")
     int insert(User user);
+
+
+    /**
+     * 查找全部
+     * @return
+     */
+    @Select("SELECT * FROM user")
+    //把数据字段映射到对象的属性，多个映射，@Result()用逗号分隔
+    @Results({
+            @Result(column = "create_time",property = "createTime")  //javaType = java.util.Date.class
+    })
+    List<User> getAll();
+
+    /**
+     * 根据id找对象
+     * @param id
+     * @return
+     */
+    @Select("SELECT * FROM user WHERE id = #{id}")
+    @Results({
+            @Result(column = "create_time",property = "createTime")
+    })
+    User findById(Long id);
+
+    /**
+     * 更新对象名称
+     * @param user
+     */
+    @Update("UPDATE user SET name=#{name} WHERE id =#{id}")
+    void update(User user);
+
+    /**
+     * 根据id删除用户
+     * @param userId
+     */
+    @Delete("DELETE FROM user WHERE id =#{userId}")
+    void delete(Long userId);
 }
 
