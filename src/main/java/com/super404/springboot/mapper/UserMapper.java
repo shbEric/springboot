9257db475646b6232ac040cef0e9545212b8fed4
shbEric <1289878918@qq.com>
2019-11-26 10:14:57 +0800

    @Results注解可以添加唯一标识，后面可以用@ResultMap简写

--- src/main/java/com/super404/springboot/mapper/UserMapper.java
+++ src/main/java/com/super404/springboot/mapper/UserMapper.java
@@ -15,7 +15,6 @@ import java.util.List;
  * 访问数据库的接口
  */
 public interface UserMapper {
-
     //推荐使用#{},会被预编译为？,在用值替换；不推荐使用${}，预编译为"xxxx"，存在sql注入风险
     @Insert("INSERT INTO user(name,phone,create_time,age) VALUES(#{name}, #{phone}, #{createTime}, #{age})")
     //获取数据库自增id，keyProperty是JAVA对象的属性，keyColumn表示数据库的字段
@@ -29,7 +28,9 @@ public interface UserMapper {
      */
     @Select("SELECT * FROM user")
     //把数据字段映射到对象的属性，多个映射，@Result()用逗号分隔
-    @Results({
+    @Results(
+            id="userMap",
+            value = {
             @Result(column = "create_time",property = "createTime")  //javaType = java.util.Date.class
     })
     List<User> getAll();
